728a96ede7b3999817e8312f89633cd817249c92
parnain <p.kiani1988@gmail.com>
2022-05-22 21:42:26 +0430

    commit

--- PamirAccounting/Commons/Enums/Settings.cs
+++ PamirAccounting/Commons/Enums/Settings.cs
@@ -111,8 +111,80 @@ namespace PamirAccounting.Commons.Enums
             Update = 2,
             [Description("حذف")]
             Delete = 3,
-     
-          
+
+
+        }
+
+
+        public enum Permission
+        {
+
+            NewAccount = 1,
+            PayAndReciveCash = 2,
+            DeletePayAndReciveCash = 3,
+            Transfer = 4,
+            DeleteTransfer = 5,
+            PayAndReciveBank = 6,
+            DeletePayAndReciveBank = 7,
+            ShippingOrder = 8,
+            DeleteShippingOrder = 9,
+            WarrantsPayable = 10,
+            DeleteWarrantsPayable = 11,
+            RecivedDocument = 12,
+            DeleteRecivedDocument = 13,
+            DepositDocument = 14,
+            DeleteDepositDocument = 15,
+            BuyCurrency = 16,
+            DeleteBuyCurrency = 17,
+            SellCurrency = 18,
+            DeleteSellCurrency = 19,
+            Customers = 20,
+            DeleteCustomers = 21,
+            ShowAccount = 22,
+            UnkwonReciveBank = 23,
+            DeleteUnkwonReciveBank = 24,
+            OprationUnkwonReciveBank = 25,
+            ExecuteDaraft = 26,
+            Balance = 27,
+            Rate = 28,
+            SareHesab = 29,
+            DeleteSareHesab = 30,
+            VosolDaryafti=31,
+            DeleteVosolDaryafti=32,
+            VagozariAsnad=33,
+            DeleteVagozariAsnad=34,
+            OdatVagozarShode=35,
+            DeleteOdatVogozarShode=36,
+            BrgashtDaryafti=37,
+            DeleteBargashtDaryafti=38,
+            OdatSareHesab=39,
+            DeleteOdatSareHesab=40,
+            OdatDaryaft=41,
+            DeleteOdatDaryafti=42,
+            PasPardakhti=43,
+            DeletePasPardakhti=44,
+            BargashtPardakhti=45,
+            DeleteBargashtPardakhti=46,
+            OdatPardakhti=47,
+            DeleteOdatPardakhti=48,
+            Contact=49,
+            DeleteContact=50,
+            Currency=51,
+            DeleteCurrency=52,
+            Group=53,
+            DeleteGroup=54,
+            Bank=55,
+            DeleteBank=56,
+            Agency=57,
+            DeleteAgency=58,
+            CurrencyAgencies=59,
+            DeleteCurrencyAgencies=60,
+            Header=61,
+            Settings=62,
+            Users=63,
+         
+
+
         }
     }
 }
